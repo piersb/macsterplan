5e8caa16094428be17b24da28eeb6eacb82cfd7f
Piers Beckley <piers@fatpigeons.com>
2015-06-29 13:40:19 +0100

    Initial Commit

---
+++ Macsterplan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Macsterplan
+//
+//  Created by Piers Beckley on 29/06/2015.
+//  Copyright (c) 2015 Piers Beckley. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Macsterplan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Macsterplan
+//
+//  Created by Piers Beckley on 29/06/2015.
+//  Copyright (c) 2015 Piers Beckley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
