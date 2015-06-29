7b80a1b47e1552298c517505962e6f962eb1373d
Piers Beckley <piers@fatpigeons.com>
2015-06-29 14:51:46 +0100

    names and bios for characters exist and are editable

---
+++ Macsterplan/Character.swift
@@ -0,0 +1,13 @@
+//
+//  Character.swift
+//  Macsterplan
+//
+//  Created by Piers Beckley on 29/06/2015.
+//  Copyright (c) 2015 Piers Beckley. All rights reserved.
+//
+
+import Cocoa
+
+class Character: NSObject {
+
+}
